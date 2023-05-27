8e6f3f20fa7559d7421bec97d3484e2ce9c9b661
tianea2160 <rhlehfndvkd7557@gmail.com>
2023-05-27 16:36:56 +0900

    feat : 댓글 필드 추가

--- src/main/kotlin/com/jaranda/goorlebackend/domain/accommodations/dto/AccommodationReadDTO.kt
+++ src/main/kotlin/com/jaranda/goorlebackend/domain/accommodations/dto/AccommodationReadDTO.kt
@@ -4,6 +4,7 @@ import com.jaranda.goorlebackend.domain.accommodations.entity.Accommodation
 import com.jaranda.goorlebackend.domain.accommodations.entity.FeatureValue
 import com.jaranda.goorlebackend.domain.accommodations.entity.Position
 import com.jaranda.goorlebackend.domain.accommodations.entity.TagValue
+import com.jaranda.goorlebackend.domain.comment.dto.CommentReadDTO
 
 class AccommodationReadDTO(
     val id: String,
@@ -12,17 +13,19 @@ class AccommodationReadDTO(
     val position: Position,
     val images: List<String>,
     val tags: List<TagValue>,
-    val features: List<FeatureValue>
+    val features: List<FeatureValue>,
+    val comments: List<CommentReadDTO>,
 ) {
     companion object {
-        fun of(entity: Accommodation): AccommodationReadDTO = AccommodationReadDTO(
+        fun of(entity: Accommodation) = AccommodationReadDTO(
             id = entity.id,
             name = entity.name,
             location = entity.location,
             position = entity.position,
             images = entity.images.map { image -> image.url },
             tags = entity.tags.map { tag -> tag.value },
-            features = entity.features.map { feature -> feature.value }
+            features = entity.features.map { feature -> feature.value },
+            comments = entity.comments.map { comment -> CommentReadDTO.of(comment) }
         )
     }
 }
