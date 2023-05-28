a9f011736e4ae782c54c6b19e153fc5c106f8306
tianea2160 <rhlehfndvkd7557@daum.net>
2023-05-28 19:14:16 +0900

    feat : 레디스 의존성 버그 수정 및 추가

--- src/main/kotlin/com/jaranda/goorlebackend/domain/auth/service/AuthService.kt
+++ src/main/kotlin/com/jaranda/goorlebackend/domain/auth/service/AuthService.kt
@@ -4,13 +4,16 @@ import com.jaranda.goorlebackend.domain.auth.dto.LoginDTO
 import com.jaranda.goorlebackend.domain.auth.dto.LoginSuccessDTO
 import com.jaranda.goorlebackend.domain.user.service.UserService
 import com.jaranda.goorlebackend.shared.security.JwtService
+import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.Duration
 
 @Service
 class AuthService(
     private val userService: UserService,
     private val jwtService: JwtService,
+    private val redisTemplate: StringRedisTemplate,
 ) {
     @Transactional
     fun login(dto: LoginDTO): LoginSuccessDTO {
@@ -18,13 +21,15 @@ class AuthService(
         userService.findUserByIdOrNull(userId) ?: userService.createUser(userId)
         val accessToken = jwtService.createAccessToken(userId)
         val refreshToken = jwtService.createRefreshToken(userId)
+        redisTemplate.opsForValue().set(userId, refreshToken, Duration.ofMillis(jwtService.refreshDuration))
         return LoginSuccessDTO(accessToken, refreshToken)
     }
 
     fun logout(userId: String) {
+        redisTemplate.delete(userId)
     }
 
-    fun secession(userId:String){
+    fun secession(userId: String) {
         userService.deleteUser(userId)
     }
 }
