63c0566646b9136083d636d3610b27f0938cb841
tianea2160 <rhlehfndvkd7557@gmail.com>
2023-05-29 14:17:22 +0900

    fix : 로그인 로직 관련 내용 일부 수정

--- src/main/kotlin/com/jaranda/goorlebackend/domain/auth/service/AuthService.kt
+++ src/main/kotlin/com/jaranda/goorlebackend/domain/auth/service/AuthService.kt
@@ -4,7 +4,7 @@ import com.jaranda.goorlebackend.domain.auth.dto.LoginDTO
 import com.jaranda.goorlebackend.domain.auth.dto.LoginSuccessDTO
 import com.jaranda.goorlebackend.domain.user.service.UserService
 import com.jaranda.goorlebackend.shared.security.JwtService
-import org.springframework.data.redis.core.StringRedisTemplate
+//import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.Duration
@@ -13,7 +13,7 @@ import java.time.Duration
 class AuthService(
     private val userService: UserService,
     private val jwtService: JwtService,
-    private val redisTemplate: StringRedisTemplate,
+//    private val redisTemplate: StringRedisTemplate,
 ) {
     @Transactional
     fun login(dto: LoginDTO): LoginSuccessDTO {
@@ -21,12 +21,12 @@ class AuthService(
         userService.findUserByIdOrNull(userId) ?: userService.createUser(userId)
         val accessToken = jwtService.createAccessToken(userId)
         val refreshToken = jwtService.createRefreshToken(userId)
-        redisTemplate.opsForValue().set(userId, refreshToken, Duration.ofMillis(jwtService.refreshDuration))
+//        redisTemplate.opsForValue().set(userId, refreshToken, Duration.ofMillis(jwtService.refreshDuration))
         return LoginSuccessDTO(accessToken, refreshToken)
     }
 
     fun logout(userId: String) {
-        redisTemplate.delete(userId)
+//        redisTemplate.delete(userId)
     }
 
     fun secession(userId: String) {
